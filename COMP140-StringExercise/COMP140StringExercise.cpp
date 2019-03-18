390578e0af185939e34bac02fca23064257b627d
Joachim <joachim.r.4k@gmail.com>
2019-03-18 15:44:27 +0000

    Finished the string exercise.

--- COMP140-StringExercise/COMP140StringExercise.cpp
+++ COMP140-StringExercise/COMP140StringExercise.cpp
@@ -15,11 +15,46 @@ public:
 	}
 	~CharacterStats()
 	{
-
+		
 	}
 
-	//Everything public! Bad, make sure its private
-	//and we use functions to access!
+	std::string GetName()
+	{
+		return Name;
+	}
+	void SetName(std::string newName)
+	{
+		Name = newName;
+	}
+
+	int GetAge()
+	{
+		return Age;
+	}
+	void SetAge(int newAge)
+	{
+		Age = newAge;
+	}
+
+	int GetHealth()
+	{
+		return Health;
+	}
+	void SetHealth(int newHealth)
+	{
+		Health = newHealth;
+	}
+
+	int GetScore()
+	{
+		return Score;
+	}
+	void SetScore(int newScore)
+	{
+		Score = newScore;
+	}
+
+private:
 	std::string Name;
 	int Age;
 	int Health;
@@ -32,18 +67,22 @@ int main()
 {
 	//Create an instance of CharacterStats
 	CharacterStats stats;
-	stats.Age = 30;
-	stats.Name = "Brian";
-	stats.Health = 100;
-	stats.Score = 0;
+	stats.SetAge(30);
+	stats.SetName("Brian");
+	stats.SetHealth(100);
+	stats.SetScore(0);
 
-	//Build a string to represent the character - to_string - http://www.cplusplus.com/reference/string/to_string/
-	std::string characterString = "Name: " + stats.Name + " Age: " + std::to_string(stats.Age) + " Health: " + std::to_string(stats.Health) + " Score: " + std::to_string(stats.Score);
+	std::stringstream characterString;
+	characterString << "Name: " << stats.GetName();
+	characterString << " Age: " << std::to_string(stats.GetAge());
+	characterString << " Health: " << std::to_string(stats.GetHealth());
+	characterString << " Score: " << std::to_string(stats.GetScore());
 
 	//Print out the string
 	std::cout << "Character Details" << std::endl;
 	std::cout << "=================" << std::endl;
-	std::cout << characterString << std::endl;
+	std::cout << characterString.str() << std::endl;
+
     return 0;
 }
 
